2c538f663b86f6b7db3fdad35cdc66653d080d44
Nan <nanftw@gmail.com>
2021-06-18 19:07:06 -0700

    Add OneSignal to app.

--- app/src/main/java/com/example/myfirstandroidapp_java/MainActivity.java
+++ app/src/main/java/com/example/myfirstandroidapp_java/MainActivity.java
@@ -7,12 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.onesignal.OneSignal;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstandroidapp_java.MESSAGE";
+    private static final String ONESIGNAL_APP_ID = "547645cb-f35e-42ed-94e4-f04ca3cdfdea";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
         setContentView(R.layout.activity_main);
     }
 
